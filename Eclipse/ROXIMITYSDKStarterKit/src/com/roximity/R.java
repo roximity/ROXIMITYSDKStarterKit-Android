4cd3a6d967c0d57637fd4a2d37eb3fbf426a49da
Danny Pier <bigonthepigdan@gmail.com>
2015-06-30 16:54:54 -0600

    Updating eclipse project to latest jar

--- Eclipse/ROXIMITYSDKStarterKit/src/com/roximity/R.java
+++ Eclipse/ROXIMITYSDKStarterKit/src/com/roximity/R.java
@@ -4,4 +4,9 @@ public final class R {
     public static final class raw {
         public static int roxstore=com.roximity.roximitysdkstarterkit.R.raw.roxstore;
     }
+    
+    public static final class menu{
+    	public static final int menu_roxwebview =com.roximity.roximitysdkstarterkit.R.menu.menu_roxwebview;
+    }
+    
 }
